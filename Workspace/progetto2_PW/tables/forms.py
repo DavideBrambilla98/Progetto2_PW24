28ce053bddd98834dca59d5b07554b2b506eb379
MarcoAlgisi <150244472+MarcoAlgisi@users.noreply.github.com>
2024-07-24 15:47:27 +0200

    update crud

--- Workspace/progetto2_PW/tables/forms.py
+++ Workspace/progetto2_PW/tables/forms.py
@@ -1,10 +1,32 @@
 from django import forms
-from .models import RicoveroTable, PatologiaTable
+from .models import RicoveroTable, PatologiaTable, PersoneTable, OspedaleTable
 
 
 class RicoveroTableForm(forms.ModelForm):
 
-    codPatologia = forms.ModelChoiceField(queryset=PatologiaTable.objects.all(), required=False)
+    paziente = forms.ModelChoiceField(
+        queryset= PersoneTable.objects.all(),
+        required=True,
+        empty_label='Seleziona paziente',
+        to_field_name='paziente',
+        widget=forms.Select
+    )
+
+    codiceOspedale = forms.ModelChoiceField(
+        queryset=OspedaleTable.objects.all(),
+        required=True,
+        empty_label='Seleziona Ospedale',
+        to_field_name='codiceOspedale',
+        widget=forms.Select
+    )
+
+    codPatologia = forms.ModelChoiceField(
+        queryset=PatologiaTable.objects.all(),
+        required=True,
+        empty_label= "Seleziona patologia",
+        to_field_name='codPatologia',
+        widget=forms.Select
+    )
     class Meta:
         model = RicoveroTable
         fields = '__all__'
